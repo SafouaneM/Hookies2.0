32f4c2086114e07388d79bbc27179377bc59678d
SafouaneM <safouane_m@Hotmail.nl>
2017-09-05 09:47:19 +0200

    Project

---
+++ IdeaProjects/Tell/src/Db.java
@@ -0,0 +1,16 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Db extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.drawString("Safouane", 50, 60);
+        g.setColor(Color.red);
+        g.drawString("Messaoudi", 60, 70);
+    }
+}
