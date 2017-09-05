cf6b35f2bb45c60050e9ceac367e2364e49315ba
SafouaneM <safouane_m@Hotmail.nl>
2017-09-05 14:12:50 +0200

    Vlag is nu gefixt ;)

--- IdeaProjects/Opdrachten H4/src/h4/H43/Vlad.java
+++ IdeaProjects/Opdrachten H4/src/h4/H43/Vlad.java
@@ -9,15 +9,15 @@ public class Vlad extends Applet {
 
     public void paint(Graphics g) {
         setBackground(Color.lightGray);
-        g.drawRect(80,170,10,80);
+        g.fillRect(80,170,10,80);
         g.setColor(Color.red);
-        g.drawRect(90,105,10,85);
-        g.fillRect(90,105,10,85);
+        g.drawRect(90,170,85,10);
+        g.fillRect(90,170,85,10);
         g.setColor(Color.white);
-        g.drawRect(100,105,10,85);
-        g.fillRect(100,105,10,85);
+        g.drawRect(90,180,85,10);
+        g.fillRect(90,180, 85,10);
         g.setColor(Color.blue);
-        g.drawRect(110,105,10,85) ;
-        g.fillRect(110,105,10,85);
+        g.drawRect(90,190,85,10) ;
+        g.fillRect(90,190,85,10);
     }
 }
