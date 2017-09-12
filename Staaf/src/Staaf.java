5b9c29ce267b0c3d5043366efba08f5147d8e88f
Safouane <31611670+SafouaneM@users.noreply.github.com>
2017-09-12 09:23:32 +0200

    Add files via upload

---
+++ Staaf/src/Staaf.java
@@ -0,0 +1,35 @@
+
+import java.awt.*;
+import java.applet.*;
+
+public class Staaf extends Applet {
+
+    private int max = 100;
+    private int min = 0;
+    private int[] ppl = {40, 90, 80, 35};
+    private String[] names = {"Valerie","Jeroen", "Hans", "Piet"};
+
+    public void init() {
+        int w = ppl.length * 80;
+        if (w > 1000) {
+            w = 1000;
+        }
+        this.setSize(w + 35, 140);
+    }
+
+    public void paint(Graphics g) {
+        int i = 0;
+        for (int p : ppl) {
+            int offset_x = 10 + i * 80;
+            g.setColor(Color.YELLOW);
+            g.fillRect(offset_x,10,30,100);
+            g.drawRect(offset_x, 10, 30, 100);
+            g.drawString(names[i], offset_x, 122);
+            g.setColor(Color.white);
+            g.fillRect(offset_x,  10, 30, 100-p);
+            i++;
+        }
+    }
+
+}
+
